15c8e97d9814609c5550422fed727ef163085b87
anouchk <analutzky@gmail.com>
2018-01-11 10:04:17 +0100

    Force single objects : ici pour éviter plusieurs PDO() et donc connexions à la bdd, je fais un objet qui ne se crée que s'il n'existait pas avant

--- serviceclasses_etc/lib/Container.php
+++ serviceclasses_etc/lib/Container.php
@@ -4,19 +4,25 @@ class Container {
 
 	private $configuration;
 
+	private  $pdo;
+
 	public function __construct (array $configuration) {
 			$this->configuration = $configuration ;
 	}
 
 	public function getPDO() {
 
-		$pdo = new PDO (
-		    $this->configuration['db_dsn'],
-		    $this->configuration['db_user'],
-		    $this->configuration['db_pass']
-		);
+		if ($this->pdo === null) {
+
+			$this->pdo = new PDO (
+			    $this->configuration['db_dsn'],
+			    $this->configuration['db_user'],
+			    $this->configuration['db_pass']
+			);
+			$this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		}
 
 		return $pdo;
-
 	}
+
 }
