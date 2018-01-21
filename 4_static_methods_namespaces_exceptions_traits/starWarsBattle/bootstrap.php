332b7160ec248a1fd4314b44cdd63f69401550a5
anouchk <analutzky@gmail.com>
2018-01-22 00:24:21 +0100

    composer autoloading

--- 4_static_methods_namespaces_exceptions_traits/starWarsBattle/bootstrap.php
+++ 4_static_methods_namespaces_exceptions_traits/starWarsBattle/bootstrap.php
@@ -1,17 +1,9 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+
 $configuration = array(
     'db_dsn'  => 'mysql:host=localhost;dbname=oo_battle2',
     'db_user' => 'root',
     'db_pass' => 'root',
 );
-
-spl_autoload_register(function($className) {
-	// we replace the backslash with a forward slash
-	$path = __DIR__.'/lib/'.str_replace('\\','/', $className).'.php';
-
-	if(file_exists($path)) {
-		require $path;
-	}
-});
-
